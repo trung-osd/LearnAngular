96534e8e9dced20b12a599145b7ad3d2bcae6d75
Trung-OSD <trung.phan@orientsoftware.com>
2022-06-05 22:02:58 +0700

    NgFor

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,6 +8,15 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   public name = 'Trung';
   public age = 22;
+  public fruits = ['Cam', "Quýt", "Mận", "Ổi", "Đào"]
+
+  public fruitsObj = [
+    {name: 'Táo', price: 10, sale: true},
+    {name: 'Quýt', price: 11, sale: false},
+    {name: 'Mận', price: 12, sale: true},
+    {name: 'Ổi', price: 13, sale: false},
+    {name: 'Đào', price: 14, sale: true},
+  ]
 
   constructor() { }
 
